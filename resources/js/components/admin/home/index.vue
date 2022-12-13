b92ebcf2219109189a145983d6012ad02f166229
hasibulabir <hasibul.abir@gmail.com>
2022-12-13 10:11:26 +0600

    first day

---
+++ resources/js/components/admin/home/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1>Welcome to Admin overview dashboard</h1> 
+    <div>
+        <router-link to="/">Home page</router-link>
+    </div>    
+</template>
---
+++ resources/js/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+//admin
+import homeAdminIndex from '../components/admin/home/index.vue'
+//pages
+import homePageIndex from '../components/pages/home/index.vue'
+//login
+import login from '../components/auth/login.vue'
+
+import notFound from '../components/notFound.vue'
+
+const routes =[
+    //admin
+    {
+        path :'/admin/home',
+        component: homeAdminIndex
+    },
+    //pages
+    {
+        path :'/',
+        component: homePageIndex
+    },
+    //login
+    {
+        path :'/login',
+        component: login
+    },
+    //notFound
+    {
+        path :'/:pathMatch(.*)*',
+        component: notFound
+    },
+
+] 
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+export default router
