5c73f6402250904305a76e91f2d0d746ea0e4c94
hasibulabir <hasibul.abir@gmail.com>
2022-12-17 12:01:59 +0600

    css

--- resources/js/components/auth/login.vue
+++ resources/js/components/auth/login.vue
@@ -6,20 +6,20 @@
                 <br>
                 <input type="password" placeholder="Enter your Password" />
                 <br>
-                <input type="submit" value="Login" />   
+                <input type="submit" value="Login" class="submit"  />   
             </form>
         </div>
     </div>
 </template>
 
 <style>
-    *{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        font-family: 'Fira sans', sans-serif;
-        font-size: 16px;
-    }
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Fira sans', sans-serif;
+    font-size: 16px;
+}
     .login{
         background: #6c5ce7;
         padding: 0;
@@ -35,6 +35,41 @@
         margin: 0 auto 0 auto;
         overflow: hidden;
     }
-
+    input{
+        background: rgba(228, 232,243, 0.8);
+        background-position: 0.5em 0.6em;
+        border: none;
+        color: rgba(80, 80 , 80, 1);
+        padding: 0 0 0 4rem;
+        margin: 0 0 1em 0;
+        width: 20em;
+        height: 2.8em;
+        outline: none;
+        transition: background-color 0.4s;
+    }
+    input:hover{
+        background-color: rgba(255, 255 , 255 , 255);
+    }
+    input:focus{
+        background-color: rgba(255, 255 , 255 , 255);
+    }
+    .submit{
+        
+        background: rgba(220, 220, 220, 1);
+        padding: 0;
+        margin: 2.5em 0 0 5em;
+        width: 10em;
+        text-transform: uppercase;
+        cursor: pointer;
+        transition: background-color 0.4s;
+    }
+    .submit:hover{
+        background: #43467f;
+        color: #ffffff;
+    }
+    .submit:focus{
+        background: #43467f;
+        color: #ffffff;
+    }
 
 </style>
